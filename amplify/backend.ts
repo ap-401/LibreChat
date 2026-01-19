e55136c6ea63a31ec1ae70370626fa27c557b7e0
ap-401 <aperez88@gmail.com>
2026-01-19 15:52:37 -0500

    Added custom resources to the backend.ts to create a vpc and DocumentDB serverless cluster.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,37 @@
 import * as documentDb from 'aws-cdk-lib/aws_docdb';
+import * as ec2 from 'aws-cdk-lib/aws_ec2';
 import { defineBackend } from '@aws-amplify/backend';
 
-defineBackend({});
+const backend = defineBackend({});
+
+const customResourceStack = backend.createStack('DocumentDbCustomResources');
+
+// Create a new VPC to deploy the document DB server with subnets
+const LibreChatVpc = new ec2.Vpc(customResourceStack, 'LibreChatVpc', {
+  vpcName: 'LibreChatVpc',
+  ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+  subnetConfiguration: [
+    {
+       cidrMask: 26,
+       name: 'data-subnet-1',
+       subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+     },
+     {
+       cidrMask: 26,
+       name: 'data-subnet-2',
+       subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+     }
+  ]
+});
+// Create the Document DB cluster in the VPC
+new documentDb.DatabaseCluster(customResourceStack, 'LibreChatDatabase', {
+  masterUser: {
+    username: 'admin',
+  },
+  vpc: LibreChatVpc,
+  serverlessV2ScalingConfiguration: {
+    minCapacity: 0.5,
+    maxCapacity: 1,
+  },
+  engineVersion: '5.0.0', // Serverless requires engine version 5.0.0 or higher
+});
